945c3af6b96f69c7676c91cbb6bf52c47ec301fb
sujal sharma <asujalsharma@gmail.com>
2024-10-22 15:21:42 +0530

    s

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(session({
 app.use(cors());
 
 app.use("/",(req,res)=>{
-  res.send("Hello")
+  res.send("Hello");
 })
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
