11e298649efc01f6ada66a9ed8548e6502dfd66a
sujal sharma <asujalsharma@gmail.com>
2024-10-22 15:16:51 +0530

    s

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.use(session({
 }));
 app.use(cors());
 
+app.use("/",(req,res)=>{
+  res.send("Hello")
+})
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error(err));
